30bcbb64b82c18f87de4292ad6a7501f2eeb4fbb
matix78 <matckolejfan@gmail.com>
2024-05-18 00:51:48 +0200

    zmiany

--- commands/General/reminder.js
+++ commands/General/reminder.js
@@ -6,7 +6,6 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('przypomnienie')
         .setDescription('Przypomina o konwoju')
-        //.setDefaultMemberPermissions(Discord.PermissionFlagsBits.Administrator)
         .addUserOption(option =>
             option
                 .setName('osoba')
@@ -23,35 +22,25 @@ module.exports = {
         const member = interaction.options.getUser('osoba');
         const role = interaction.options.getRole('rola');
 
-        if (message && !member && !role) {
+        if (!member && !role) {
             return interaction.reply({ content: 'Nie wybrano ani użytkownika, ani roli do której chcesz wysłać wiadomość!', ephemeral: true });
         }
+        const customId = `euro-speed-message-modal:${member ? member.id : 'none'}:${role ? role.id : 'none'}`;
 
-        if (message && member) {
-            const user = await interaction.guild.members.fetch(member.id);
+        const modal = new Discord.ModalBuilder()
+            .setCustomId(customId)
+            .setTitle('Wprowadź dane')
+            .addComponents(
+                new Discord.ActionRowBuilder().addComponents(
+                    new Discord.TextInputBuilder()
+                        .setCustomId('espl-message')
+                        .setLabel('Wiadomość')
+                        .setPlaceholder('Wprowadź tu swoją treść wiadomości którą chcesz wysłać')
+                        .setStyle(Discord.TextInputStyle.Paragraph)
+                        .setRequired(true)
+                )
+            );
 
-            if (user) {
-                try {
-                    await user.send(message);
-                } catch (error) {
-                    interaction.reply({ content: 'Wiadomość nie mogła zostać wysłana do użytkownika ponieważ nie przyjmuje on wiadomości od nieznanych osób!', ephemeral: true });
-                }
-                return interaction.reply({ content: `Wiadomość została wysłana do użytkownika <@${member.id}>`, ephemeral: true });
-            } else {
-                return interaction.reply({ content: 'Nie znaleziono użytkownika na serverze!', ephemeral: true });
-            }
-        } else if (message && role) {
-            const membersWithRole = interaction.guild.members.cache.filter(member => member.roles.cache.has(role.id));
-            membersWithRole.forEach(async member => {
-                try {
-                    await member.send(message);
-                } catch (error) {
-                    console.error(`Nie udało się wysłać wiadomości do ${member.user.tag}: ${error}`);
-                }
-            });
-            return interaction.reply({ content: `Wiadomość wysłana do członków z rolą ${role.name}`, ephemeral: true });
-
-            return interaction.reply({ content: 'Wystąpił błąd podczas wysyłania wiadomości.', ephemeral: true });
-        }
+        await interaction.showModal(modal);
     }
-}
+};
--- index.js
+++ index.js
@@ -38,6 +38,11 @@ for (const folder of fs.readdirSync('./commands')) {
 
 client.once('ready', () => {
     console.log('Bot discord jest online!');
+    client.user.setStatus('dnd');
+    client.user.setPresence({
+        activities: [{ name: 'Euro Truck Simulator 2', type: Discord.ActivityType.Playing }],
+        status: 'dnd'
+    });
 });
 
 client.on('interactionCreate', async interaction => {
@@ -53,6 +58,40 @@ client.on('interactionCreate', async interaction => {
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: 'Wystąpił błąd w komendzie!', ephemeral: true });
+        }
+    }
+
+
+    if (interaction.isModalSubmit()) {
+        const [modalName, memberId, roleId] = interaction.customId.split(':');
+
+        if (modalName === 'euro-speed-message-modal') {
+            const message = interaction.fields.getTextInputValue('espl-message');
+
+            if (memberId !== 'none') {
+                try {
+                    const user = await interaction.guild.members.fetch(memberId);
+                    await user.send(message);
+                    return interaction.reply({ content: `Wiadomość została wysłana do użytkownika <@${memberId}>`, ephemeral: true });
+                } catch (error) {
+                    return interaction.reply({ content: 'Wiadomość nie mogła zostać wysłana do użytkownika ponieważ nie przyjmuje on wiadomości od nieznanych osób!', ephemeral: true });
+                }
+            }
+
+            if (roleId !== 'none') {
+                const role = await interaction.guild.roles.fetch(roleId);
+                const membersWithRole = interaction.guild.members.cache.filter(member => member.roles.cache.has(roleId));
+                membersWithRole.forEach(async member => {
+                    try {
+                        await member.send(message);
+                    } catch (error) {
+                        console.error(`Nie udało się wysłać wiadomości do ${member.user.username}: ${error}`);
+                    }
+                });
+                return interaction.reply({ content: `Wiadomość wysłana do członków z rolą <@&${role.id}>`, ephemeral: true });
+            }
+
+            return interaction.reply({ content: 'Wystąpił błąd podczas wysyłania wiadomości.', ephemeral: true });
         }
     }
 });
